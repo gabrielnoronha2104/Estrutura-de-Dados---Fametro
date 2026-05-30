e4e79779e7d5f46deb3841272191f9a5e7d35117
gabrielnoronha2104 <gabriel.brasil.noronha@gmail.com>
2026-05-29 21:08:38 -0400

    Create parte_2.c

---
+++ parte_2.c
@@ -0,0 +1,26 @@
+1. Qual das seguintes afirmações sobre ponteiros em C é VERDADEIRA?
+
+Resposta Correta
+
+b) Um ponteiro armazena o endereço de memória de uma variável.
+
+2. O que o operador * (asterisco) faz quando usado na frente de uma variável ponteiro?
+
+Resposta Correta
+
+d) Acessa o valor armazenado no endereço para o qual o ponteiro aponta.
+
+3. Para que serve a função malloc() da biblioteca <stdlib.h>?
+
+Resposta Correta
+
+c) Para alocar um bloco de memória e retornar um ponteiro para ele.
+
+4. Como seria a chamada correta da função abaixo?
+
+void modifica(int *a, int b)
+
+Resposta Correta
+int numero = 5;
+
+modifica(&numero, 10);
